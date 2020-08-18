0d82cfa5621ff8063f414cfb9dc06a89edbb76c3
Arthur Figueiredo <fig16009@gmail.com>
2020-08-18 10:17:49 -0600

    Added SLM Prototype

---
+++ 2020/SLM Prototype/script.js
@@ -0,0 +1,73 @@
+
+
+
+function optionOne() {
+
+    var optionOne = document.getElementById('option-one');
+    var optionTwo = document.getElementById('option-two');
+    var optionThree = document.getElementById('option-three');
+    var optionOneContent = document.getElementById('option-one-content');
+    var optionTwoContent = document.getElementById('option-two-content');
+    var optionThreeContent = document.getElementById('option-three-content');
+
+    optionOne.style.backgroundColor = "#2677B9";
+    optionOne.style.color = "white";
+    
+    optionTwo.style.backgroundColor = "#EAEAEA";
+    optionTwo.style.color = "#707070";
+
+    optionThree.style.backgroundColor = "#EAEAEA";
+    optionThree.style.color = "#707070";
+
+    optionOneContent.style.display = "flex";
+    optionTwoContent.style.display = "none";
+    optionThreeContent.style.display = "none";
+}
+
+function optionTwo() {
+
+    var optionOne = document.getElementById('option-one');
+    var optionTwo = document.getElementById('option-two');
+    var optionThree = document.getElementById('option-three');
+    var optionOneContent = document.getElementById('option-one-content');
+    var optionTwoContent = document.getElementById('option-two-content');
+    var optionThreeContent = document.getElementById('option-three-content');
+
+    optionTwo.style.backgroundColor = "#2677B9";
+    optionTwo.style.color = "white";
+    
+    optionOne.style.backgroundColor = "#EAEAEA";
+    optionOne.style.color = "#707070";
+
+    optionThree.style.backgroundColor = "#EAEAEA";
+    optionThree.style.color = "#707070";
+
+    optionOneContent.style.display = "none";
+    optionTwoContent.style.display = "flex";
+    optionThreeContent.style.display = "none";
+    
+}
+
+function optionThree() {
+
+    var optionOne = document.getElementById('option-one');
+    var optionTwo = document.getElementById('option-two');
+    var optionThree = document.getElementById('option-three');
+    var optionOneContent = document.getElementById('option-one-content');
+    var optionTwoContent = document.getElementById('option-two-content');
+    var optionThreeContent = document.getElementById('option-three-content');
+
+    optionThree.style.backgroundColor = "#2677B9";
+    optionThree.style.color = "white";
+    
+    optionOne.style.backgroundColor = "#EAEAEA";
+    optionOne.style.color = "#707070";
+
+    optionTwo.style.backgroundColor = "#EAEAEA";
+    optionTwo.style.color = "#707070";
+
+    optionOneContent.style.display = "none";
+    optionTwoContent.style.display = "none";
+    optionThreeContent.style.display = "flex";
+    
+}
